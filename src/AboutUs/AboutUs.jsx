fdab1e74f1e3a9ef721521908d2417d87f7c3db5
yana <yana.gizdova1@gmail.com>
2023-06-19 23:34:04 +0200

    com

--- src/AboutUs/AboutUs.jsx
+++ src/AboutUs/AboutUs.jsx
@@ -4,6 +4,7 @@
 export default function AboutUs() {
   document.title = "About Us";
   return <>
+<<<<<<< Updated upstream
     <h1>About Us</h1>
     <p>Survival on the Moon is a web page representation of NASA's group dynamic about survival on the Moon.</p>
     <p>For this, each group must make decisions to survive on the Moon.</p>
@@ -18,5 +19,19 @@ export default function AboutUs() {
       <li>Sara Rodríguez Rojo: <a href="https://www.linkedin.com/in/sara-rodriguez-rojo/">https://www.linkedin.com/in/sara-rodriguez-rojo/</a></li>
       <li>Yana Nikolaeva Gizdova: <a href="https://www.linkedin.com/in/yana-nikolaeva-gizdova/">https://www.linkedin.com/in/yana-nikolaeva-gizdova/</a></li>
     </ul>
+=======
+  <h1>About Us</h1>
+  <div style={{position: 'absolute', top: 170, left: 30, textAlign: "justify", flex:'0.75', fontSize: '1.2em'}}>
+    <p>This page was developed by 4 students from the Polytechnic University of Madrid.</p>
+    <p>For more information about the project, you can contact us at the following linkedIn addresses: </p>
+    <p>Carlos Nieto Petinal: <a href="https://www.linkedin.com/in/carlosnietopetinal/">https://www.linkedin.com/in/carlosnietopetinal/</a></p>
+    <p>César Agulló Quirós: <a href="https://www.linkedin.com/in/c%C3%A9sar-agull%C3%B3-a299a622b/">https://www.linkedin.com/in/c%C3%A9sar-agull%C3%B3-a299a622b/</a></p>
+    <p>Sara Rodríguez Rojo: <a href="https://www.linkedin.com/in/sara-rodriguez-rojo/">https://www.linkedin.com/in/sara-rodriguez-rojo/</a></p>
+    <p>Yana Nikolaeva Gizdova: <a href="https://www.linkedin.com/in/yana-nikolaeva-gizdova/">https://www.linkedin.com/in/yana-nikolaeva-gizdova/</a></p>
+    <div>
+      <img style={{position: 'absolute', top: 270, left: -15, width: 300, height: 150}} src={imgUrl}  />
+    </div>
+  </div>
+>>>>>>> Stashed changes
   </>
 }
--- src/MainView.jsx
+++ src/MainView.jsx
@@ -6,12 +6,14 @@ const MainView = () => {
   return (
     <>
       <div style={{ display: 'flex' }}>
-        <div style={{ flex:'0.75', fontSize: '2em', textAlign: 'justify', margin: '0.5em' }}>
-          You are a member of a space crew originally scheduled to rendezvous with a mother ship on the lighted surface of the moon. However, due to mechanical difficulties, your ship was forced to land at a spot some 200 miles from the rendezvous point. During reentry and landing, much of the equipment aboard was damaged and, since survival depends on reaching the mother ship, the most critical items available must be chosen for the 200-mile trip. Below are listed the 15 items left intact and undamaged after landing. Your task is to rank order them in terms of their importance for your crew in allowing them to reach the rendezvous point. Place the number 1 by the most important item, the number 2 by the second most important, and so on through number 15 for the least important.
+        <div style={{ flex:'0.75', fontSize: '1.6em', textAlign: 'justify', margin: '0.5em' }}>
+          You are a member of a space crew originally scheduled to rendezvous with a mother ship on the lighted surface of the moon.
+          However, due to mechanical difficulties, your ship was forced to land at a spot some 200 miles from the rendezvous point. During reentry and landing, much of the equipment aboard was damaged and, since survival depends on reaching the mother ship, the most critical items available must be chosen for the 200-mile trip.
+          <br/><br/>Below are listed the 15 items left intact and undamaged after landing. Your task is to rank order them in terms of their importance for your crew in allowing them to reach the rendezvous point. Place the number 1 by the most important item, the number 2 by the second most important, and so on through number 15 for the least important.
         </div>
       </div>
       <Link to="/local_dynamic">
-        <button style={{fontSize:'3em'}}>
+        <button style={{fontSize:'3em', color: 'white'}}>
           Start Locally<br />
           and Individually
         </button>
